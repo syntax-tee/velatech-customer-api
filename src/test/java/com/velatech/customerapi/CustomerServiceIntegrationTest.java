11c71e5acb006773fa8ba0633de930e3a8ac9dde
Emmanuel003 <taiye.o@e360africa.com>
2019-10-22 22:05:44 +0100

    --added snippets to create database

--- src/test/java/com/velatech/customerapi/CustomerServiceIntegrationTest.java
+++ src/test/java/com/velatech/customerapi/CustomerServiceIntegrationTest.java
@@ -1,32 +1,32 @@
-package com.velatech.customerapi;
-
-
-import com.velatech.customerapi.entities.Customer;
-import com.velatech.customerapi.response.CustomerResponse;
-import com.velatech.customerapi.respository.CustomerRepository;
-import com.velatech.customerapi.service.CustomerService;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
-public class CustomerServiceIntegrationTest {
-
-
-    @Autowired
-    CustomerService customerService;
-
-
+//package com.velatech.customerapi;
+//
+//
+//import com.velatech.customerapi.entities.Customer;
+//import com.velatech.customerapi.response.CustomerResponse;
+//import com.velatech.customerapi.respository.CustomerRepository;
+//import com.velatech.customerapi.service.CustomerService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+//public class CustomerServiceIntegrationTest {
+//
+//
 //    @Autowired
-//    CustomerRepository customerRepository;
+//    CustomerService customerService;
 //
 //
-//    long bin = 45717360;
-//    CustomerResponse customerResponse  =  customerService.verifyCardService(bin);
+////    @Autowired
+////    CustomerRepository customerRepository;
+////
+////
+////    long bin = 45717360;
+////    CustomerResponse customerResponse  =  customerService.verifyCardService(bin);
+////
+////
 //
-//
-
-}
+//}
